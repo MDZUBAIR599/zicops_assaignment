e7c06fdc6063626b381b6d19fdee0c731d104799
MDZUBAIR599 <https://github.com/MDZUBAIR599>
2023-01-14 17:35:18 +0530

    first commit

---
+++ src/Components/User.jsx
@@ -0,0 +1,86 @@
+import React, { useEffect, useState } from "react";
+import "./User.css";
+function User() {
+  // time limit for lab access
+  const timeLimit = 10;
+  // state to keep track of remaining time
+  const [remainingTime, setRemainingTime] = useState(timeLimit);
+  // state to keep track of error message
+  const [ErrorMessage, SetErrorMessage] = useState("");
+
+  // state to keep track of user Login
+  const [UsreLoggedIn, setUsreLoggedIn] = useState(false);
+
+  //User Password
+  const user = {
+    password: "zubair123",
+  };
+  const HandleSubmit = (event) => {
+    const UerPassword = event.target.password.value;
+    console.log(UerPassword);
+    event.preventDefault();
+    if ("zubair123" === user.password) {
+      setUsreLoggedIn(true);
+    } else {
+      SetErrorMessage(
+        "you have entered an incorrect password. Please try again"
+      );
+    }
+  };
+
+  // exit button event handler
+  const handleExit = () => {
+    setUsreLoggedIn(false);
+    setRemainingTime(timeLimit);
+  };
+
+  // timer function
+  useEffect(() => {
+    if (UsreLoggedIn) {
+      const intervalId = setInterval(() => {
+        setRemainingTime((prevRemainingTime) => prevRemainingTime - 1);
+        if (remainingTime === 0) {
+          handleExit();
+        }
+      }, 2000);
+      return () => clearInterval(intervalId);
+    }
+  }, [UsreLoggedIn, remainingTime, handleExit]);
+
+  return (
+    <>
+      <div className="main">
+        <div className="MainCircle">
+          {UsreLoggedIn ? (
+            <div className="lab">
+              <div>
+                <div className="left_door"></div>
+                <h1>Timer:<span style={{ fontsize: "20px" ,}}>{remainingTime}</span> </h1>
+                
+                <button onClick={handleExit}>Exit</button>
+                <div className="right_door"></div>
+              </div>
+            </div>
+          ) : (
+            <div >
+               <div className="finger_print">
+                <img src="https://user-images.githubusercontent.com/101566430/212470407-3cda4116-ae8b-4a8c-8946-41c1b2161049.jpeg" alt="fingerprint" />
+               </div>
+              <div className="formbox">
+               
+               <form onSubmit={HandleSubmit}>
+                 <label>ENTER YOUR PILEARNING PASSWORD</label>
+                 <input className="password" type="password" name="password" />
+                 <br />
+                 <input className="submit" type="submit" value="Enter Lab" />
+               </form>
+             </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default User;
